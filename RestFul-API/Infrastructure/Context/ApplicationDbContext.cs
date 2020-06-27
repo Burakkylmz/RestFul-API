3fecbd6ca58ac13be723615a097cf82a8f66908c
Burak Yılmaz <burakk.ylmaz@outlook.com>
2020-06-27 15:14:44 +0300

    Application Db Context created

---
+++ RestFul-API/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using RestFul_API.Infrastructure.Entities.Concrete;
+
+
+namespace RestFul_API.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
