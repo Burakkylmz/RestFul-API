fad42bf2f4c6ecfe14459da6e7e0098361cc53d3
Burak Yılmaz <burakk.ylmaz@outlook.com>
2020-06-27 15:09:16 +0300

    Create the base entity structure

---
+++ RestFul-API/Infrastructure/Entities/Abstract/IEntity.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+
+namespace RestFul_API.Infrastructure.Entities.Abstract
+{
+
+    public enum Status
+    {
+        None,
+        Active,
+        Modified,
+        Passive
+    }
+
+    public interface IEntity<T>
+    {
+        T Id { get; set; }
+
+        DateTime CreateDate { get; set; }
+        DateTime? UpdateDate { get; set; }
+        DateTime? DeleteDate { get; set; }
+
+        Status Status { get; set; }
+    }
+}
---
+++ RestFul-API/Infrastructure/Entities/Concrete/KernelEntity.cs
@@ -0,0 +1,23 @@
+﻿
+using RestFul_API.Infrastructure.Entities.Abstract;
+using System;
+
+
+namespace RestFul_API.Infrastructure.Entities.Concrete
+{
+    public class KernelEntity : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+
+        private DateTime _createDate = DateTime.Now;
+        public DateTime CreateDate { get { return _createDate; } set { _createDate = value; } }
+
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+
+
+
+        private Status _status = Status.Active;
+        public Status Status { get { return _status; } set { _status = value; } }
+    }
+}
